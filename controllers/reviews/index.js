92f70711b8ae765ae211fe1afcef99ab3b13460f
krizs981 <krizs_lopez@hotmail.com>
2022-03-13 12:17:37 +0100

    cambio en index para eliminar edit review

--- controllers/reviews/index.js
+++ controllers/reviews/index.js
@@ -1,9 +1,7 @@
 const newReview = require('./newReview');
-const editReview = require('./editReview');
 const deleteReview = require('./deleteReview');
 
 module.exports = {
     newReview,
-    editReview,
     deleteReview,
 };
