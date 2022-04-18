1f7f9a9c87dc7a3ef67436b4b1f38977cf3531fe
MartinGG <agoladamartin@gmail.com>
2022-04-18 20:10:54 +0200

    creating get business profile whit reviews

--- controllers/users/getBusiness.js
+++ controllers/users/getBusiness.js
@@ -10,14 +10,21 @@ const getBusiness = async (req, res, next) => {
         const { idUser } = req.params;
 
         // Obtenemos el id del usuario que realiza la request.
-        const idReqUser = req.userAuth.id;
+        /* const idReqUser = req.userAuth.id; */
 
         // Obtenemos todos los datos que me interesan de la empresa de la cuál
         // se solicita información.
 
         const [business] = await connection.query(
-            `SELECT name, description, headquarter, sector, url_web FROM business WHERE idUser = ?`,
+            `SELECT id, name, description, headquarter, sector, url_web FROM business WHERE idUser = ?`,
             [idUser]
+        );
+
+        /* Obtenemos todas reviews de la empresa */
+
+        const [businessReviews] = await connection.query(
+            `SELECT * FROM review WHERE idBusiness = ?`,
+            [business[0].id]
         );
 
         // Objeto con la información básica de la empresa.
@@ -28,6 +35,7 @@ const getBusiness = async (req, res, next) => {
             url_web: business[0].url_web,
             headquarter: business[0].headquarter,
             sector: business[0].sector,
+            reviews: businessReviews,
         };
 
         // Obtenemos todos los datos registrados en la tabla de usuario/empresa del cuál
@@ -48,12 +56,19 @@ const getBusiness = async (req, res, next) => {
         // Si el usuario que realiza la request es el dueño de dicho usuario/empresa o si es
         // un administrador vamos a agregar información extra.
 
-        if (users[0].id === idReqUser || req.userAuth.role === 'admin') {
+        /* if (users[0].id === idReqUser || req.userAuth.role === 'admin') {
             userInfo.email = users[0].email;
             userInfo.role = users[0].role;
             userInfo.createdAt = users[0].createdAt;
-        }
 
+            res.send({
+                status: 'ok',
+                data: {
+                    businessInfo: businessInfo,
+                    userInfo: userInfo,
+                },
+            });
+        } else { */
         res.send({
             status: 'ok',
             data: {
